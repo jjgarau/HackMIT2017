3eee261be239382d13e9c31cb97586771529329b
CarlosGR <carlosgr@mit.edu>
2017-09-17 02:09:17 -0400

    fix in kmeans

--- rect_Kmeans.py
+++ rect_Kmeans.py
@@ -26,15 +26,16 @@ def get_orient_xy(thetas, dists):
 
 def Kmeans(vert_rects, horiz_rects, n_vert, n_horiz, max_coords, niter=10, ntimes=10):
 
-    xs = np.random.uniform(low=0, high=max_coords[0], size=n_vert)
-    ys = np.random.uniform(low=0, high=max_coords[1], size=n_horiz)
-
     clusters_hs = {}
     clusters_vs = {}
 
-    for exec in ntimes:
+    for exec in range(ntimes):
 
-        dist = 0.0
+        xs = np.random.uniform(low=0, high=max_coords[0], size=n_vert)
+        ys = np.random.uniform(low=0, high=max_coords[1], size=n_horiz)
+
+        dist_v = 0.0
+        dist_h = 0.0
         clusters_h = np.zeros(shape=(len(horiz_rects), 2)) # asign each point to a cluster
         clusters_v = np.zeros(shape=(len(vert_rects), 2))
         for iter in niter:
@@ -42,12 +43,12 @@ def Kmeans(vert_rects, horiz_rects, n_vert, n_horiz, max_coords, niter=10, ntime
             for i, x in enumerate(xs):
                 d = vert_rects - x
                 clusters_v[d < clusters_v[:,1], :] = [i, d]
-            dist += np.sum(clusters_v[:,1] * clusters_v[:,1])
+            dist_v += np.sum(clusters_v[:,1] * clusters_v[:,1])
 
             for i, y in enumerate(ys):
                 d = horiz_rects - y
                 clusters_h[d < clusters_h[:,1], :] = [i, d]
-            dist += np.sum(clusters_h[:,1] * clusters_h[:,1])
+            dist_h += np.sum(clusters_h[:,1] * clusters_h[:,1])
 
             # expectation
             xs = np.mean(vert_rects[clusters_v[:,0] == xs])
@@ -57,28 +58,35 @@ def Kmeans(vert_rects, horiz_rects, n_vert, n_horiz, max_coords, niter=10, ntime
         key_h = tuple(clusters_h[:,0])
 
         if not key_v in clusters_vs:
-            clusters_v[key_v] = (0, 0.0)
-        if not key_h in clusters_hs:
-            clusters_h[key_h] = (0, 0.0)
-        clusters_vs[key_v][0] += 1
-        clusters_vs[key_v][1] += dist
-        clusters_hs[key_h][0] += 1
-        clusters_vs[key_v][1] += dist
+            clusters_vs[key_v] = (1, xs, dist_v)
+        elif dist_v < clusters_vs[key_v][2]:
+            clusters_vs[key_v] = (clusters_vs[key_v][0]+1, xs, dist_v)
+        else:
+            clusters_vs[key_v][0] += 1
 
-    clusters_v, n_max, avg_dist_v = [], 0, 0.0
-    for cluster, (n, d) in clusters_vs.items():
+        if not key_h in clusters_hs:
+            clusters_hs[key_h] = (1, ys, dist_h)
+        elif dist_h < clusters_hs[key_h][2]:
+            clusters_hs[key_h] = (clusters_hs[key_h][0]+1, ys, dist_h)
+        else:
+            clusters_hs[key_h][0] += 1
+
+    clusters_v, n_max, xs, dist_v = [], 0, [], 0.0
+    for cluster, (n, x, d) in clusters_vs.items():
         if n > n_max:
             n_max = n
             clusters_v = cluster
-            avg_dist_v = d *1. / n
+            xs = x
+            dist_v = d
 
-    clusters_h, n_max, avg_dist_h = [], 0, 0.0
-    for cluster, (n, d) in clusters_hs.items():
+    clusters_h, n_max, ys, avg_dist_h = [], 0, [], 0.0
+    for cluster, (n, y, d) in clusters_hs.items():
         if n > n_max:
             n_max = n
             clusters_h = cluster
-            avg_dist_h = d * 1. / n
+            ys = y
+            dist_h = d
 
-    return clusters_v, clusters_h, avg_dist_v, avg_dist_h
+    return clusters_v, clusters_h, xs, ys, dist_v, dist_h
 
 
