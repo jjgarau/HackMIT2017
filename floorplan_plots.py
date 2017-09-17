16e82e75904203fa35300fc99ddac0f651e9d95e
AlvaroGI <31722017+AlvaroGI@users.noreply.github.com>
2017-09-17 01:36:14 -0400

    Hotspots implemented

--- floorplan_plots.py
+++ floorplan_plots.py
@@ -1,29 +1,70 @@
 ###########################################################################
-# IMPORT
+# IMPORT LIBRARIES AND MAP
 ###########################################################################
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-#from scipy.misc import imread                   # Cargo imread de scipy.misc
+from scipy.misc import imread                   # Cargo imread de scipy.misc
 import matplotlib.mlab as mlab
 from PIL import Image
+
+imagen_superficial = Image.open('33_4b2.png')
+size_img = imagen_superficial.size
 
 ###########################################################################
 # DATA
 ###########################################################################
 
 # Grafo (nodos en orden)
-nodos_conec = [[1],[2,3]] # Conectar cada nodo como minimo consigo mismo
-nodos_x = np.array([75,80,35,55])
-nodos_y = np.array([20,185,185,235])
+nodos_conec = [[1],[2,3],[2],[4],[4]] # Conectar cada nodo como minimo consigo mismo
+nodos_x = np.array([75,80,35,55,50])
+nodos_y = np.array([20,185,185,235,300])
+
+N = len(nodos_conec) # Numero total de nodos
 
 # Camino optimo
 nodos_opt = [0,1,3]
 
 ###########################################################################
+# HOTSPOTS DISTRIBUTION
+###########################################################################
+Nh = 3 # Number of hotspots per floor
+D = [1,3,2] # Density of people connected to each hotspot
+mindis = 0.1 # Minimum distance between hotspots factor
+h0 = np.random.randint(0,N)
+xh0 = nodos_x[h0]
+yh0 = nodos_y[h0]
+
+h1 = np.random.randint(0,N)
+xh1 = nodos_x[h1]
+yh1 = nodos_y[h1]
+l01 = ((xh1-xh0)**2+(yh0-yh1)**2)**0.5
+while (l01 < (size_img[1]*size_img[0])**0.5*mindis):
+    h1 = np.random.randint(0,N)
+    xh1 = nodos_x[h1]
+    yh1 = nodos_y[h1]
+    l01 = ((xh1-xh0)**2+(yh0-yh1)**2)**0.5
+
+
+h2 = np.random.randint(0,N)
+xh2 = nodos_x[h2]
+yh2 = nodos_y[h2]
+l02 = ((xh2-xh0)**2+(yh0-yh2)**2)**0.5
+l12 = ((xh2-xh1)**2+(yh2-yh1)**2)**0.5
+while (l02 < (size_img[1]*size_img[0])**0.5*mindis) or (l12 < (size_img[1]*size_img[0])**0.5*mindis):
+    h2 = np.random.randint(0,N)
+    xh2 = nodos_x[h2]
+    yh2 = nodos_y[h2]
+    l02 = ((xh2-xh0)**2+(yh0-yh2)**2)**0.5
+    l12 = ((xh2-xh1)**2+(yh2-yh1)**2)**0.5
+
+
+xh = [xh0,xh1,xh2]
+yh = [yh0,yh1,yh2]
+
+###########################################################################
 # PLOT FONDO (MAPA)
 ###########################################################################
-imagen_superficial = Image.open('33_4b.png')
 # Creo una figura
 plt.figure()
 # Muestro la imagen en pantalla
@@ -32,7 +73,6 @@ plt.imshow(imagen_superficial)
 ###########################################################################
 # PLOT GRAFO
 ###########################################################################
-N = len(nodos_conec) # Numero total de nodos
 
 # Aristas
 for ii in range(0,N):
@@ -56,36 +96,46 @@ for ii in nodos_opt[0:-1]:
 # Nodos
 plt.plot(nodos_x,nodos_y,'or')
 
-#plt.plot(0,0,'ok') #<-- plot a black point at the origin
-plt.axis('equal')  #<-- set the axes to the same scale
-#plt.xlim([-1,5]) #<-- set the x axis limits
-#plt.ylim([-1,5]) #<-- set the y axis limits
-plt.grid(b=True, which='major') #<-- plot grid lines
-
-
 ###########################################################################
 # PLOT HOTSPOTS
 ###########################################################################
 matplotlib.rcParams['xtick.direction'] = 'out'
 matplotlib.rcParams['ytick.direction'] = 'out'
 
-delta = 0.025
-sizex = imagen_superficial.size[0]
-sizey = imagen_superficial.size[1]
+for jj in range(0,Nh-1):
+    delta = 0.025
+    sizex = imagen_superficial.size[0]
+    sizey = imagen_superficial.size[1]
 
-# Factors
-xfactor = 78.5
-yfactor = 79.6
-meansize = (sizex+sizey)/2
-mufactor = 400
+    if D[jj] == 1:
+        colorr = 'Greens'
+    elif D[jj] == 2:
+        colorr = 'Blues'
+    else:
+        colorr = 'Reds'
 
-x = np.arange(-sizex/xfactor, sizex/xfactor, delta)
-y = np.arange(-sizey/yfactor, sizey/yfactor, delta)
-X, Y = np.meshgrid(x, y)
+    # Factors
+    xfactor = 80
+    yfactor = 80
+    meansize = (sizex+sizey)/2
+    mufactor = 400
 
-Z1 = mlab.bivariate_normal(X, Y, meansize/mufactor, meansize/mufactor, 0, 0)
+    x = np.arange(-sizex/xfactor, sizex/xfactor, delta)
+    y = np.arange(-sizey/yfactor, sizey/yfactor, delta)
+    X, Y = np.meshgrid(x, y)
+
+    asd = 40
+    Z1 = mlab.bivariate_normal(X, Y, meansize/mufactor, meansize/mufactor, (xh[jj]-sizex/2)/asd, (yh[jj]-sizey/2)/asd)
+
+    plt.imshow(Z1, alpha=0.5, cmap=colorr)
+
+print(xh)
+print(yh)
 
 
-plt.imshow(Z1, alpha=0.7, cmap='YlOrBr')
-
+#plt.plot(xh,yh,'og') #<-- plot a black point at the origin
+plt.axis('equal')  #<-- set the axes to the same scale
+#plt.xlim([-1,5]) #<-- set the x axis limits
+#plt.ylim([-1,5]) #<-- set the y axis limits
+plt.grid(b=True, which='major') #<-- plot grid lines
 plt.show()
