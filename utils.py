06df7978b9a42430af31a94202a23992aafaf34a
Aleix <aleixpb2@gmail.com>
2017-09-17 09:04:27 -0400

    find_edges

--- utils.py
+++ utils.py
@@ -6,3 +6,15 @@ def cartesian(listx, listy):
             ret.append((x,y))
     return ret
 
+def find_edges(coords):
+    edges = []
+    lgth = len(coords)
+    for i in range(lgth):
+        for j in range(i+1, lgth):
+            ci = coords[i]
+            cj = coords[j]
+            if (ci[0] == cj[0]) ^ (ci[1] == cj[1]):
+                edges.append((i,j))
+    return edges
+
+print(find_edges([(1,1),(4,1),(1,5),(5,5)]))
