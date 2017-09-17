90eeb8e5db9110500521d597f8b8d45128731b49
Aleix <aleixpb2@gmail.com>
2017-09-16 23:14:08 -0400

    Better structure

--- json_to_graph.py
+++ json_to_graph.py
@@ -1,22 +1,35 @@
 import json
-from pprint import pprint
 import math
+import graph
 
 def dist(x1, y1, x2, y2):
     dx = x2 - x1
     dy = y2 - y1
     return math.sqrt(dx*dx + dy*dy)
 
-with open('coords.txt') as data_file:
-    data = json.load(data_file)
+def genStructure(data):
     nnodes = len(data["coords"])
     structure = [[] for i in range(nnodes)]
     for edge in data["adj"]:
         a = edge[0]
         b = edge[1]
-        d = dist( data["coords"][a][0], data["coords"][a][1], data["coords"][b][0],
-                  data["coords"][b][1])
+        d = dist(data["coords"][a][0], data["coords"][a][1], data["coords"][b][0],
+                 data["coords"][b][1])
         structure[a].append([b, d])
+    return structure
 
-    print(structure)
+def main():
+    with open('coords.txt') as data_file:
+        data = json.load(data_file)
+
+    structure = genStructure(data)
+    gr = graph.Graph(structure)
+    path, dista = gr.get_dijkstra_path(0, 18)
+    print(path)
+    print(dista)
+
+if __name__ == "__main__":
+    main()
+
+
 
